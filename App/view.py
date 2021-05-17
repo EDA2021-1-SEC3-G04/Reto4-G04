00144f08ad97efb8d0412deb6c633876bb263f03
mar-cas3 <61592394+mar-cas3@users.noreply.github.com>
2021-05-17 16:24:20 -0500

    menu view

--- App/view.py
+++ App/view.py
@@ -22,6 +22,7 @@
 
 import config as cf
 import sys
+import threading
 import controller
 from DISClib.ADT import list as lt
 assert cf
@@ -30,29 +31,96 @@ assert cf
 """
 La vista se encarga de la interacción con el usuario
 Presenta el menu de opciones y por cada seleccion
-se hace la solicitud al controlador para ejecutar la
+se hace la solicitud al catalogrolador para ejecutar la
 operación solicitada
 """
 
 def printMenu():
+    print("\n")
+    print("*******************************************")
     print("Bienvenido")
     print("1- Cargar información en el catálogo")
-    print("2- ")
+    print("2- Ecatalograr clústers (components conectados)")
+    print("3- Ecatalograr puntos de interconexión")
+    print("4- Ecatalograr ruta minima entre paises")
+    print("5- Identificar infraestructura crítica")
+    print("6- Impacto de fallo en un landing point")
+
+
+def optionTwo(catalog):
+    "Req 1"
+    pass
+
+
+def optionThree(catalog):
+    "Req 2"
+    pass
+
+
+def optionFour(catalog):
+    "Req 3"
+    pass
+
+
+def optionFive(catalog):
+    "Req 4"
+    pass
+
+
+def optionSix(catalog):
+    "Req 5"
+    pass
+
+
+def optionSeven(catalog):
+    "Req 6 - Bono"
+    pass
+
+
+def optionEight(catalog):
+    "Req 7 - Bono"
+    pass
 
 catalog = None
 
-"""
-Menu principal
-"""
-while True:
-    printMenu()
-    inputs = input('Seleccione una opción para continuar\n')
-    if int(inputs[0]) == 1:
-        print("Cargando información de los archivos ....")
 
-    elif int(inputs[0]) == 2:
-        pass
+def thread_cycle():
+    while True:
+        printMenu()
+        inputs = input('Seleccione una opción para cataloginuar\n')
+        if int(inputs[0]) == 1:
+            print("Cargando información de los archivos ....")
+            catalog = controller.loadData()
 
-    else:
-        sys.exit(0)
-sys.exit(0)
+        elif int(inputs[0]) == 2:
+            pass
+
+        elif int(inputs[0]) == 3:
+            optionThree(catalog)
+
+        elif int(inputs[0]) == 4:
+            
+            optionFour(catalog)
+
+        elif int(inputs[0]) == 5:
+            optionFive(catalog)
+
+        elif int(inputs[0]) == 6:
+            optionSix(catalog)
+
+        elif int(inputs[0]) == 7:
+            optionSeven(catalog)
+        
+        elif int(inputs[0]) == 8:
+            optionEight(catalog)
+
+        else:
+            sys.exit(0)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    threading.stack_size(67108864)  # 64MB stack
+    sys.setrecursionlimit(2 ** 20)
+    thread = threading.Thread(target=thread_cycle)
+    thread.start()
